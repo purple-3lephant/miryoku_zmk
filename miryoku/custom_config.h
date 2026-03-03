92698d47d944523c526c533a26dacbe26fb8bf4c
Nick Pytlinski <nick@pytlinski.org>
2026-03-02 20:43:10 -0500

    Add Totem dongle support with QWERTY Miryoku
    - New totem_dongle shield: mock kscan + full matrix transform
    - Kconfig: dongle is BLE central (2 peripherals), halves are peripherals
    - config/totem_dongle.keymap for dongle-specific keymap lookup
    - build.yaml targeting dongle + left + right + settings_reset
    - miryoku/custom_config.h: MIRYOKU_ALPHAS_QWERTY
    - Simplified GitHub Actions using ZMK user-build-matrices workflow

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Use QWERTY as the base alpha layer instead of the default Colemak Mod-DH.
+#define MIRYOKU_ALPHAS_QWERTY
